0fbc3e602bb96643ff559f1f195b0b4e0e7cc093
Josep Monjo <jvmonjo@gmail.com>
2025-10-06 13:09:43 +0200

    ADDED: sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n', '@nuxtjs/sitemap'],
 
   devtools: {
     enabled: true
   },
 
   css: ['~/assets/css/main.css'],
+
+  site: {
+    url: 'https://bonairepatacona.com',
+    name: 'Bonaire Patacona. Apartamentos turísticos en la playa de la Patacona, Valencia'
+  },
 
   ui: {
     theme: {
@@ -42,7 +47,7 @@ export default defineNuxtConfig({
   },
 
   i18n: {
-    strategy: 'prefix_except_default',
+    strategy: 'prefix_and_default',
     locales: [
       { code: 'en', iso: 'en-US', name: 'English' },
       { code: 'es', iso: 'es-ES', name: 'Español' },
